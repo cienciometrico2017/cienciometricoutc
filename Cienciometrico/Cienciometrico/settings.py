cd4f2d8e323edc83e960f58929d53c5537f69c4d
dmoyaUTC <danilomoya19@gmail.com>
2018-05-13 15:22:13 -0500

    comit inicial

--- Cienciometrico/Cienciometrico/settings.py
+++ Cienciometrico/Cienciometrico/settings.py
@@ -121,9 +121,9 @@ WSGI_APPLICATION = 'Cienciometrico.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'cienciometrico',
+        'NAME': 'cienciometric',
         'USER': 'postgres',
-        'PASSWORD': 'drex',
+        'PASSWORD': '1945',
         'HOST': 'localhost',
         'PORT': 5432,
     }
